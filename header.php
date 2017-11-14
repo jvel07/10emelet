2e6b74b684a4e2f85f4ccb0e98d6b5417f58b054
Laska-HUN <berczy.balint@gmail.com>
2017-11-15 00:05:21 +0100

    test upload

---
+++ header.php
@@ -0,0 +1,6 @@
+	<div class="masthead clearfix">
+        <div class="inner">
+            <h3 class="masthead-brand">Advertising</h3> <!-- logo comes here-->
+            <?php include 'menu.php';?> <!-- guest menu-->
+		</div>
+	</div>
---
+++ menu.php
@@ -0,0 +1,7 @@
+<nav>
+    <ul class="nav masthead-nav"><!-- it will be dinamically generated-->
+		<li class="active"><a href="#">Page1</a></li>
+        <li><a href="user.php">Page2</a></li>
+		<li><a href="advertisement.php">Page3</a></li>
+    </ul>
+</nav>
