594fff53cf052a70093cf78aa354f2f79b4d2af7
jvel07 <jose@Joses-MacBook.local>
2017-11-14 23:34:59 +0100

    index

--- mysqli_connection.php
+++ mysqli_connection.php
@@ -1,5 +1,5 @@
 <?php
- $link = mysqli_connect('127.0.0.1','root','','advertising');
+ $link = mysqli_connect('192.168.1.119','root','','advertising');
 if(!$link ){
     echo "Connection failed: " . mysqli_connect_error();
     echo "Errno: " . mysqli_connect_errno();
