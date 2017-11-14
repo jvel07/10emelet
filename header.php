bcc37a42968055d99ef689bbb438c9fbbc3bc354
Laska-HUN <berczy.balint@gmail.com>
2017-11-15 00:12:34 +0100

    Add files via upload

--- header.php
+++ header.php
@@ -1,6 +1,6 @@
 	<div class="masthead clearfix">
         <div class="inner">
-            <h3 class="masthead-brand">Advertising</h3> <!-- logo comes here-->
+            <h3 class="masthead-brand">Welcome to the Umbrella Corp.</h3> <!-- logo comes here-->
             <?php include 'menu.php';?> <!-- guest menu-->
 		</div>
 	</div>
