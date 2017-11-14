ee43acccc17aba145f675b035a2beb89c7f4b59f
Paksifules95 <33672389+Paksifules95@users.noreply.github.com>
2017-11-15 00:06:32 +0100

    Add files via upload

--- user.php
+++ user.php
@@ -1,3 +1,44 @@
+<?php
+session_start();
+
+if(isset($_SESSION['u_id'])!="") {
+	header("Location: index.php");
+}
+
+include_once 'mysqli_connection.php';
+
+//check if form is submitted
+if (isset($_POST['login'])) {
+
+	$email = mysqli_real_escape_string($con, $_POST['email']);
+	$password = mysqli_real_escape_string($con, $_POST['password']);
+	$result = mysqli_query($con, "SELECT * FROM users WHERE email = '" . $email. "' and password = '" . ($password) . "'");
+
+	if ($row = mysqli_fetch_array($result)) {
+		$_SESSION['usr_id'] = $row['id'];
+		$_SESSION['usr_name'] = $row['name'];
+		header("Location: index.php");
+	} else {
+		$errormsg = "Incorrect Email or Password!!!";
+	}
+}
+if (isset($_POST['name'])) {
+
+	$name = mysqli_real_escape_string($con, $_POST['name']);
+	$password = mysqli_real_escape_string($con, $_POST['password']);
+	$result1 = mysqli_query($con, "SELECT * FROM users WHERE name = '" . $name. "' and password = '" . ($password) . "'");
+
+	if ($row = mysqli_fetch_array($result1)) {
+		$_SESSION['usr_id'] = $row['id'];
+		$_SESSION['usr_name'] = $row['name'];
+		header("Location: index.php");
+	} else {
+		$errormsg = "Incorrect Name or Password!!!";
+	}
+}
+?>
+
+?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -42,7 +83,7 @@
                             <?php
                             require_once('mysqli_connection.php');
                             $queryUsers = "SELECT iduser, username FROM user";
-                            $response = mysqli_query($link , $queryUsers);
+                            $response = mysqli_query($con , $queryUsers);
                             ?>
                             <tbody>
                                 <?php
